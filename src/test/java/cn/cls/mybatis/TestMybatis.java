3c0215d4da26db87c974101a31c1b23e68059aef
成龙生 <1536463948@qq.com>
2022-09-07 22:19:58 +0800

    测试插入数据

---
+++ src/test/java/cn/cls/mybatis/TestMybatis.java
@@ -0,0 +1,33 @@
+package cn.cls.mybatis;
+
+import cn.cls.mybatis.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TestMybatis {
+
+    @Test
+    public void testInsert() throws IOException {
+        // 获取配置文件字节流
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        // 通过工厂构建器创建sql会话工厂
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        SqlSessionFactory factory = builder.build(is);
+        // sql会话工厂开启会话
+        SqlSession sqlSession = factory.openSession();
+        // 获取mapper实现
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        //
+        int result = mapper.insertUser();
+        // 提交事务
+        sqlSession.commit();
+        System.out.println("result = " + result);
+    }
+
+}
