a889bc3de9ac5730a5e5910dcf6c1b45254816bc
成龙生 <1536463948@qq.com>
2022-09-08 00:06:04 +0800

    添加日志功能

--- src/test/java/cn/cls/mybatis/TestMybatis.java
+++ src/test/java/cn/cls/mybatis/TestMybatis.java
@@ -19,14 +19,14 @@ public class TestMybatis {
         // 通过工厂构建器创建sql会话工厂
         SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
         SqlSessionFactory factory = builder.build(is);
-        // sql会话工厂开启会话
-        SqlSession sqlSession = factory.openSession();
+        // sql会话工厂开启会话,自动提交事务
+        SqlSession sqlSession = factory.openSession(true);
         // 获取mapper实现
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         //
         int result = mapper.insertUser();
         // 提交事务
-        sqlSession.commit();
+//        sqlSession.commit();
         System.out.println("result = " + result);
     }
 
