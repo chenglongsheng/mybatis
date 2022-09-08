740d2cc03ac88bc9c1a50dd91bd3a91557332142
成龙生 <1536463948@qq.com>
2022-09-08 11:19:12 +0800

    特殊SQL的执行

--- src/main/java/cn/cls/mybatis/mapper/UserMapper.java
+++ src/main/java/cn/cls/mybatis/mapper/UserMapper.java
@@ -9,7 +9,15 @@ import java.util.Map;
 
 public interface UserMapper {
 
-    int insertUser();
+    /**
+     * 添加用户信息
+     *
+     * @param user
+     * @return useGeneratedKeys：设置使用自增的主键
+     * keyProperty：因为增删改有统一的返回值是受影响的行数，因此只能将获取的自增的主键放在传输的参
+     * 数user对象的某个属性中
+     */
+    int insertUser(User user);
 
     int deleteUser();
 
@@ -44,5 +52,21 @@ public interface UserMapper {
      */
     @MapKey("id")
     Map<String, Object> getAllUserToMap();
+
+    /**
+     * 测试模糊查询
+     *
+     * @param param
+     * @return
+     */
+    List<User> testLike(@Param("param") String param);
+
+    /**
+     * 批量删除
+     *
+     * @param ids
+     * @return
+     */
+    int deleteMore(@Param("ids") String ids);
 
 }
